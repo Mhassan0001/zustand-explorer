c355e5d81fcbd0f5b1d299e805af9f39a4f7cfa1
Mhassan0001 <hassanfareed5050@gmail.com>
2026-06-04 21:50:37 +0500

    feat: implement login API integration with Zustand store and axios instance

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,73 @@
+import { useState } from "react";
+import useAuthStore from "../stores/useAuthStore";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const { login, error, isLoading } = useAuthStore();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const success = await login(formData.email, formData.password);
+
+    if (success) {
+      navigate("/todo");
+    }
+  };
   return (
     <>
       <div className="flex justify-center bg-custom items-center flex-col">
-        <h1 className="pt-5 text-[30px] pb-5">Please ! Login Here....</h1>
+        <h1 className="pt-5 text-[30px] tracking-[5px] pb-5">
+          Please ! Login Here....
+        </h1>
 
-        <div className="w-140 rounded-2xl h-88 bg-[#1A1A1E] s">
-          <p className="text-[25px] tracking-[3px] text-amber-100 pt-9 text-center">
-            Welcome Back!....
-          </p>
-          <p className="text-[10px] tracking-[1px] text-amber-400 text-center">
-            Sign-in to Your High Velocity workspace
-          </p>
-          <p className="text-center pt-5 py-3">
-            <input
-              placeholder="Enter Your Email..."
-              type="Email"
-              className=" border bg-[] h-12 w-70 text-amber-100 border-amber-200 rounded-[0.8rem] pl-2 "
-            />
-          </p>
-          <p className="text-center">
-            <input
-              placeholder="Enter Your Password..."
-              type="password"
-              className=" border h-12 w-70  text-amber-100 border-amber-200 rounded-[0.8rem] pl-2 "
-            />
-          </p>
+        <form onSubmit={handleSubmit}>
+          <div className="w-125 rounded-[30px] h-88 bg-[#1A1A1E] s">
+            <p className="text-[25px] tracking-[3px] text-amber-100 pt-11 text-center">
+              Welcome Back!....
+            </p>
+            <p className="text-[10px] tracking-[1px] text-amber-400 text-center">
+              Sign-in to Your High Velocity workspace
+            </p>
+            <p className="text-center pt-5 py-3">
+              <input
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter Your Email..."
+                type="email"
+                className="  bg-[#0A0A0C] h-12 w-70 text-amber-100  rounded-[0.8rem] pl-2 "
+              />
+            </p>
+            <p className="text-center">
+              <input
+                placeholder="Enter Your Password..."
+                name="password"
+                onChange={handleChange}
+                value={formData.password}
+                type="password"
+                className="  h-12 w-70 bg-[#0A0A0C]  text-amber-100  rounded-[0.8rem] pl-2 "
+              />
+            </p>
 
-          <p className="text-center pt-5">
-            <button className="w-35 h-10 bg-amber-300 rounded-[10px]">
-              Sign In
-            </button>
-          </p>
-        </div>
+            {error && <p className="text-red-600 text-center">{error}</p>}
+
+            <p className="text-center pt-5  ">
+              <button
+                type="submit"
+                className="w-35 h-10 bg-[#E2DFD2] cursor-pointer tracking-[2px] border border-black rounded-[10px]"
+              >
+                {isLoading ? "Signing In..." : "Sign In"}
+              </button>
+            </p>
+          </div>
+        </form>
       </div>
     </>
   );
--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -1,2 +1,34 @@
 import axiosInstance from "../services/axiosInstance";
 import { create } from "zustand";
+
+const useAuthStore = create((set) => ({
+  user: null,
+  error: null,
+  isLoading: false,
+
+  login: async (email, password) => {
+    set({ isLoading: true, error: null });
+
+    if (!email || !password) {
+      const errorMessage = "Email & password are Required";
+      set({ error: errorMessage, isLoading: false });
+      return false;
+    }
+
+    try {
+      const response = await axiosInstance.post("/auth/login", {
+        email,
+        password,
+      });
+
+      set({ user: response.data.data, isLoading: false });
+      return true;
+    } catch (error) {
+      const msg = error.response?.data?.msg || error.message;
+      set({ error: msg, isLoading: false });
+      return false;
+    }
+  },
+}));
+
+export default useAuthStore;
