db93228ca751686c1f18b04fcc70db9b3afb5ce0
Mhassan0001 <hassanfareed5050@gmail.com>
2026-06-24 18:33:44 +0500

    feat: add todo update functionality with inline editing

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -4,12 +4,29 @@ import useAuthStore from "../stores/useAuthStore";
 import { Link } from "react-router-dom";
 import { MdOutlineLogout } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
-
+import { MdModeEdit } from "react-icons/md";
+import { toast } from "sonner";
 const Todo = () => {
   const [input, setInput] = useState("");
-  const { isLoading, error, tasks, createTask, getTask, remove } =
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState(null);
+  const { isLoading, error, tasks, update, createTask, getTask, remove } =
     useTodoStore();
   const { user, logout } = useAuthStore();
+
+  const handleEdit = (task) => {
+    setEditId(task._id);
+    setEditText(task.task);
+    console.log(task._id);
+    toast.info([task._id," ",task.task]);
+    
+  };
+
+  const handleUpdate = async (id) => {
+    await update(id, editText);
+    setEditId(null);
+    setEditText("");
+  };
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -36,7 +53,6 @@ const Todo = () => {
           Today's Goals!
         </h1>
 
-        {/* Form */}
         <form
           onSubmit={onSubmit}
           className="flex flex-col items-center w-full max-w-md mt-4"
@@ -59,7 +75,7 @@ const Todo = () => {
             </button>
 
             <Link
-              to="/"
+              to="/count"
               className="bg-purple-900 rounded-2xl px-4 py-2 text-white tracking-wide inline-block"
             >
               Redirect
@@ -67,21 +83,55 @@ const Todo = () => {
           </div>
         </form>
 
-        <div className="w-full max-w-md mt-3">
+        <div className="w-full max-w-md  mt-3">
           {error && <p className="text-red-500 py-4 text-center">{error}</p>}
+
           <ul className="space-y-3">
             {tasks.map((task) => (
               <li key={task._id} className=" py-2 w-fit mx-auto">
-                <span className="bg-pink-400 px-10 py-2 text-white rounded-2xl ">
-                  {task.task}
-                </span>
-                <span className="pl-3">
-                <button 
-                  onClick={() => remove(task._id)}
-                  className="bg-red-500 text-white py-2  rounded-[10px] px-2"
-                >
-                  <MdDelete />
-                </button></span>
+                {editId === task._id ? (
+                  <>
+                    <input
+                      type=""
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                      placeholder="Enter"
+                      className="bg-pink-400 px-10 py-2 text-white rounded-2xl"
+                    />
+
+                    <span className="pl-1">
+                      <button
+                        className="bg-green-500 text-white px-3 py-2 rounded-2xl"
+                        onClick={() => handleUpdate(task._id)}
+                      >
+                        save
+                      </button>
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span className="bg-pink-400 px-10 py-2 text-white rounded-2xl">
+                      {task.task}
+                    </span>
+                    <span className="pl-3">
+                      <button
+                        onClick={() => remove(task._id)}
+                        className="cursor-pointer bg-red-500 text-white py-2  rounded-[10px] px-2"
+                      >
+                        <MdDelete />
+                      </button>
+                    </span>
+
+                    <span className="pl-3">
+                      <button
+                        onClick={() => handleEdit(task)}
+                        className="cursor-pointer bg-yellow-500 text-white py-2  rounded-[10px] px-2"
+                      >
+                        <MdModeEdit />
+                      </button>
+                    </span>
+                  </>
+                )}
               </li>
             ))}
           </ul>
